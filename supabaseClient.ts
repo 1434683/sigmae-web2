654358b02180ae5f4a2ffabae6ecdceb7f2d3426
1434683 <14bpmm2ciaadm@gmail.com>
2025-10-16 10:25:57 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = https://lmbwwhwjgvethgforziw.supabase.co/
+const supabaseUrl = https://lmbwwhwjgvethgforziw.supabase.co
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
